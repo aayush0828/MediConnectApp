d99d43f516fe365a175aa9c6899ca354f8e2df79
aayush0828 <aayushthakur432@gmail.com>
2026-02-01 20:28:02 +0530

    Day 6

--- server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJpa.java
@@ -1,5 +1,29 @@
 package com.edutech.progressive.service.impl;
 
-public class DoctorServiceImplJpa  {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Doctor;
+
+@Service
+public class DoctorServiceImplJpa {
+
+    public List<Doctor> getAllDoctors() {
+        return null;
+    }
+
+    public Doctor getDoctorById(int doctorId) {
+        return null;
+    }
+
+    public Integer addDoctor(Doctor doctor) {
+        return -1;
+    }
+
+    public void updateDoctor(Doctor doctor) {
+    }
+
+    public void deleteDoctor(int doctorId) {
+    }
 }
--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
@@ -2,30 +2,38 @@ package com.edutech.progressive.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.entity.Patient;
+import com.edutech.progressive.repository.PatientRepository;
 
 
 
 @Service
 public class PatientServiceImplJpa {
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     public List<Patient> getAllPatients() {
-        return null;
+        return patientRepository.findAll();
     }
 
     public Patient getPatientById(int patientId) {
-        return null;
+        return patientRepository.findByPatientId(patientId);
     }
 
     public Integer addPatient(Patient patient) {
-        return -1;
+        patientRepository.save(patient);
+        return 1;
     }
 
     public void updatePatient(Patient patient) {
+        patientRepository.save(patient);
     }
 
     public void deletePatient(int patientId) {
+        patientRepository.deleteById(patientId);
     }
 }
